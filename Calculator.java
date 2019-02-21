ae65662c7f57e82bbb0cd387c609885bd932cbd5
pauline <gkaremera@gmail.com>
2019-02-20 21:23:51 -0600

    Homework5

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+package edu.dmacc.codedsm.hw5;
+
+import java.sql.SQLOutput;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        double x = Math.sqrt(27);
+        System.out.println(x);
+        int y = 28 / 5;
+        System.out.println(y);
+        double a = Math.pow(2.2,4);
+        System.out.println(a);
+        double b = Math.random() * 10;
+        System.out.println((int)b);
+    }}
